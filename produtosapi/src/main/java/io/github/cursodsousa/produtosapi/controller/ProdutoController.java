776528059c181a2d4adcb4a6e5bf828654f8c863
Felipe Bomfim <felipe.bomfim@camara.leg.br>
2025-01-03 15:37:59 -0300

    Implemented the whole cycle of saving a product in the database with a Jpa repository

--- produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -1,20 +1,34 @@
 package io.github.cursodsousa.produtosapi.controller;
 
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.cursodsousa.produtosapi.model.Produto;
+import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
 
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar (@RequestBody Produto produto){
         System.out.println("Produto recebido: "+ produto);
+
+        var id  = UUID.randomUUID().toString();
+        produto.setId(id);
+        
+        produtoRepository.save(produto);
         return produto;
     }
 }
---
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package io.github.cursodsousa.produtosapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.cursodsousa.produtosapi.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+}
